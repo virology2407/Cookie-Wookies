ec84c8bd70316c04c8e6802685502e070f505a90
Camilo Barki <camilobarki@MacBook.local>
2020-10-18 19:43:32 -0300

    mail.php added

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['Nombre'];
+$email = $_POST['Email'];
+$type = $_POST['Motivo de Contacto'];
+$message = $_POST['mensaje'];
+$formcontent=" From: $name \n Type: $type \n Message: $message";
+$recipient = "hola@cookiewookies.me";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='gracias.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
