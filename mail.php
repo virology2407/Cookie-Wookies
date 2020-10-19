5c07b5ae87d1129ad78a6509a9f0e0faa6b29b02
Camilo Barki <camilobarki@MacBook.local>
2020-10-18 21:05:47 -0300

    form redirect on success

--- mail.php
+++ mail.php
@@ -8,5 +8,5 @@ $recipient = "hola@cookiewookies.me";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='gracias.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+header("Location: http://www.cookiewookies.me/gracias.html");
 ?>
